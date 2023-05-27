c29492217d98f951efaa708dee5e27e4453d4b5a
Amit Barletz <amit@amplication.com>
2023-05-27 18:18:29 +0300

    format

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { IconType } from "react-icons";
+import { IconType } from 'react-icons';
 
 interface ButtonProps {
   label: string;
@@ -11,26 +11,26 @@ interface ButtonProps {
   icon?: IconType;
 }
 
-const Button: React.FC<ButtonProps> = ({ 
-  label, 
-  onClick, 
-  disabled, 
+const Button: React.FC<ButtonProps> = ({
+  label,
+  onClick,
+  disabled,
   outline,
   small,
   icon: Icon,
 }) => {
-  return ( 
+  return (
     <button
       disabled={disabled}
       onClick={onClick}
       className={`
         relative
-        disabled:opacity-70
-        disabled:cursor-not-allowed
-        rounded-lg
-        hover:opacity-80
-        transition
         w-full
+        rounded-lg
+        transition
+        hover:opacity-80
+        disabled:cursor-not-allowed
+        disabled:opacity-70
         ${outline ? 'bg-white' : 'bg-rose-500'}
         ${outline ? 'border-black' : 'border-rose-500'}
         ${outline ? 'text-black' : 'text-white'}
@@ -43,16 +43,16 @@ const Button: React.FC<ButtonProps> = ({
       {Icon && (
         <Icon
           size={24}
-          className="
+          className='
             absolute
             left-4
             top-3
-          "
+          '
         />
       )}
       {label}
     </button>
-   );
-}
- 
+  );
+};
+
 export default Button;
