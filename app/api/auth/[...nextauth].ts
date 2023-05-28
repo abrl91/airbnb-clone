dc2f77f5b325c26486f1836a999508e1fa2e4d6b
Amit Barletz <amit@amplication.com>
2023-05-28 12:38:11 +0300

    next auth

---
+++ app/api/auth/[...nextauth].ts
@@ -0,0 +1,63 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { AuthOptions } from 'next-auth';
+import prisma from '@/libs/prismadb';
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import bcrypt from 'bcrypt';
+import { User } from '@prisma/client';
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'email', type: 'text' },
+        password: { label: 'password', type: 'password' },
+      },
+      async authorize(
+        credentials: Record<'email' | 'password', string> | undefined
+      ): Promise<User | null> {
+        if (!credentials?.email || !credentials.password) {
+          throw new Error('Invalid credentials');
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!user || !user?.hashedPassword) {
+          throw new Error('Invalid credentials');
+        }
+
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
+
+        if (!isCorrectPassword) {
+          throw new Error('Invalid credentials');
+        }
+
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: '/',
+  },
+  session: {
+    strategy: 'jwt',
+  },
+  secret: process.env.NEXT_AUTH_SECRET!,
+  debug: process.env.NODE_ENV === 'development',
+};
